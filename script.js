5aa9d39556395474dc93c5dc7fa6c765a580caa3
Essien Nathaniel <thaniessy@gmail.com>
2023-02-04 03:27:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//Question 1
+let arr = [[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+
+const allNum = [arr[5][0][0], arr[3], arr[5][0][3][0], arr[5][1], arr[1][0]]
+console.log(allNum)
+
+//Question 2
+let wordArray = ["javascript", "web", "Capacity Bay"];
+const sentence = `I am a ${sentence[1]} developer at ${sentence[2]} and i love ${sentence[0]}`
+console.log(sentence)
+
+//Question 3
+const word = "Generalization"
+const character = word[10]
+//const character = word.charAt(10)
+console.log(character)
+
+//Question 4
+const fruits = ["orange", "apple", "guava", "lemon"];
+fruits.unshift("pea", "banana")
+fruits.push("Avocado", "mango")
+console.log(fruits)
